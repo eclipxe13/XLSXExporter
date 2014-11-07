fb18318764313608d122961ea48179cabc3d245b
Carlos C Soto <eclipxe13@gmail.com>
2014-11-07 16:54:33 -0600

    support for date time write

--- classes/XLSXExporter/CellTypes.php
+++ classes/XLSXExporter/CellTypes.php
@@ -5,7 +5,9 @@ namespace XLSXExporter;
 abstract class CellTypes {
     const TEXT = "TEXT";
     const NUMBER = "NUMBER";
-    const TIMESTAMP = "TIMESTAMP";
     const BOOLEAN = "BOOLEAN";
     const INLINE = "INLINE";
+    const TIME = "TIME";
+    const DATE = "DATE";
+    const DATETIME = "DATETIME";
 }
