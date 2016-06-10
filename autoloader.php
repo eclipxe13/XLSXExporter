b03e6468974d7cef8b5bce444071c9501c580b66
Carlos C Soto <eclipxe13@gmail.com>
2016-06-10 09:49:46 -0500

    better autoloader code

--- autoloader.php
+++ autoloader.php
@@ -3,12 +3,12 @@
 /**
  * Include this file if you are not using composer, why aren't you using composer??
  */
-spl_autoload_register(function($classname) {
-    if (0 === strpos($classname, "XLSXExporter\\")) {
-        $filename = dirname(__DIR__) . "/classes/XLSXExporter/" . str_replace("\\", "/", $classname) . ".php";
-        if (file_exists($filename) && is_readable($filename)) {
-            require_once $filename;
-        }
+spl_autoload_register(function ($classname) {
+    if (0 !== strpos($classname, "XLSXExporter\\")) {
+        return;
+    }
+    $filename = dirname(__DIR__) . "/classes/XLSXExporter/" . str_replace("\\", "/", $classname) . ".php";
+    if (file_exists($filename) && is_readable($filename)) {
+        require_once $filename;
     }
 });
-
