0c014673e1fb4662a365c7eb6b9e342d2d5cd74d
Carlos C Soto <eclipxe13@gmail.com>
2016-06-10 13:12:22 -0500

    Document ProviderInterface methods

--- sources/XLSXExporter/ProviderInterface.php
+++ sources/XLSXExporter/ProviderInterface.php
@@ -4,8 +4,32 @@ namespace XLSXExporter;
 
 interface ProviderInterface
 {
+    /**
+     * Return a value from the current tuple
+     *
+     * @param string $key
+     * @return mixed
+     */
     public function get($key);
+
+    /**
+     * Move the provider to the next tuple
+     *
+     * @return void
+     */
     public function next();
+
+    /**
+     * Checks if current tuple is valid
+     *
+     * @return bool
+     */
     public function valid();
+
+    /**
+     * Return the total number of tuples
+     *
+     * @return int
+     */
     public function count();
 }
