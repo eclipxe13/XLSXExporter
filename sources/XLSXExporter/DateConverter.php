56af09a7d894ffe2c2ded63b3badbdb0a5353632
Carlos C Soto <eclipxe13@gmail.com>
2016-06-10 19:51:12 -0500

    rename $ts to $timestamp

--- sources/XLSXExporter/DateConverter.php
+++ sources/XLSXExporter/DateConverter.php
@@ -8,42 +8,42 @@ class DateConverter
     const DATE = 'DATE';
     const DATETIME = 'DATETIME';
 
-    public static function tsToExcel($ts, $type)
+    public static function tsToExcel($timestamp, $type)
     {
         if ($type === self::TIME) {
-            return static::tsToExcelTime($ts);
+            return static::tsToExcelTime($timestamp);
         }
         if ($type === self::DATE) {
-            return static::tsToExcelDate($ts);
+            return static::tsToExcelDate($timestamp);
         }
-        return static::tsToExcelDateTime($ts);
+        return static::tsToExcelDateTime($timestamp);
     }
 
-    public static function tsToExcelTime($ts)
+    public static function tsToExcelTime($timestamp)
     {
-        $utc = static::utcParts($ts);
+        $utc = static::utcParts($timestamp);
         return (($utc[3] * 3600) + ($utc[4] * 60) + $utc[5]) / 86400;
     }
 
-    public static function tsToExcelDate($ts)
+    public static function tsToExcelDate($timestamp)
     {
-        $utc = static::utcParts($ts);
+        $utc = static::utcParts($timestamp);
         // Bug 1900 is not a leap year
         // http://support.microsoft.com/kb/214326
         $delta = ($utc[0] == 1900 && $utc[1] <= 2) ? -1 : 0;
-        return (int) (25569 + $delta + (($ts + $utc[6]) / 86400));
+        return (int) (25569 + $delta + (($timestamp + $utc[6]) / 86400));
     }
 
-    public static function tsToExcelDateTime($ts)
+    public static function tsToExcelDateTime($timestamp)
     {
-        return (float) static::tsToExcelDate($ts) + static::tsToExcelTime($ts);
+        return (float) static::tsToExcelDate($timestamp) + static::tsToExcelTime($timestamp);
     }
 
-    public static function utcParts($ts)
+    public static function utcParts($timestamp)
     {
-        $offset = date('Z', $ts);
+        $offset = date('Z', $timestamp);
         return array_merge(
-            explode(',', gmdate('Y,m,d,H,i,s', $ts + $offset)),
+            explode(',', gmdate('Y,m,d,H,i,s', $timestamp + $offset)),
             [$offset]
         );
     }
